1c5e0d75b9897be1c742f43a162610f85a72836a
superman19993 <73101805+superman19993@users.noreply.github.com>
2021-10-13 10:45:51 +0700

    minor fix

--- Server/index.php
+++ Server/index.php
@@ -4,7 +4,6 @@ require './Controllers/BaseController.php';
 require './Core/Database.php';
 require './Models/BaseModel.php';
 
-
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
@@ -14,11 +13,8 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
-
 $controllerName= ucfirst((strtolower($uri[4])). 'Controller');
 require "./Controllers/${controllerName}.php";
-
-//if ($actionName) $controllerObject-> $actionName();
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 if (isset($uri[5])) $id = $uri[5];
